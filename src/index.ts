3a5391ae3bbb6db1f0c63e455446b14f1a22d52c
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-02-10 19:06:05 +0500

    (feat) edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-const express = require("express");
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+
+import { UserModel } from "./schemas";
+import { UserController } from "./controllers";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/chat", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
 });
+
+const User = new UserController();
+
+app.get("/users/:id", User.show);
+app.post("/users/create", User.create);
+app.delete("/users/:id", User.delete);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
